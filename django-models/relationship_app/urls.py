c29eac342a473d7269c26899ad27df950a6ff389
Epicgika <Taher.sanad2000@gmail.com>
2024-11-09 22:04:12 +0200

    Include templates path

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('library/<int:pk>/', views.LibraryDetailView.as_view(),
          name='library-detail'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Library
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import UserCreationForm
 
 
 def list_books(request):
@@ -14,3 +16,16 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+# Registration View
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to login after successful registration
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
