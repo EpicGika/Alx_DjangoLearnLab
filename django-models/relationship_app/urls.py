a4b1c2fb736e5cc57593f82966759651d8a98aa2
Epicgika <Taher.sanad2000@gmail.com>
2024-11-09 22:08:33 +0200

    Include templates path

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 from .views import list_books
 from django.contrib.auth import views as auth_views
-from django.contrib.auth import login
+
+
 urlpatterns = [
     path('library/<int:pk>/', views.LibraryDetailView.as_view(),
          name='library-detail'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register, name='register'),
 ]
