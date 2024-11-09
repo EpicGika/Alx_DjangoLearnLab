01a607c7ce17c84f6f0ad1e995c00e9480b4780a
Epicgika <Taher.sanad2000@gmail.com>
2024-11-09 20:53:32 +0200

    Editing the function

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView
 from .models import Library
-from django.views.generic.detail import DetailView
 from django.shortcuts import render
 from .models import Book
 
@@ -11,5 +12,5 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
