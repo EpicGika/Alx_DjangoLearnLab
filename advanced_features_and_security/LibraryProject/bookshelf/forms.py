5e103d2ffffbaf9a5c3b7841b06c8c595b393bc5
Taher <taher.sanad2000@gmail.com>
2024-11-17 13:51:12 +0200

    Add the project dirct

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
-class SearchForm(forms.Form):
-    query = forms.CharField(max_length=100)
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100, required=True)
+    description = forms.CharField(label='Description', widget=forms.Textarea, required=False)
+    published_date = forms.DateField(label='Published Date', required=False)
