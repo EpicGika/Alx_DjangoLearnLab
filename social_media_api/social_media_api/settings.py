09c089716e07efadb8613e18d9adf62daf035675
EpicGika <Taher.sanad2000@gmail.com>
2024-12-15 14:34:49 +0200

    Tyring deploy

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -129,6 +129,7 @@ DEBUG = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
